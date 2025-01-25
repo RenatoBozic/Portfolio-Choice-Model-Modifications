2e0f21ce481f0f351884a82d47928f7021687e9c
Renato Božič <78423919+RenatoBozic@users.noreply.github.com>
2025-01-25 17:14:13 +0100

    Create PortfolioCoiceModel4_0_ReturnFn.m

---
+++ Model 31/PortfolioCoiceModel4_0_ReturnFn.m
@@ -0,0 +1,14 @@
+function F=PortfolioCoiceModel4_0_ReturnFn(savings,riskyshare,a,z1,z2,e1,e2,w,sigma,agej,Jr,pension,kappa_j)
+
+F=-Inf;
+if agej<Jr 
+    c=w*kappa_j*z1*e1+a-savings; % Working ages
+else 
+    c=pension*z2*e2+a-savings; % Retirement ages
+end
+
+if c>0
+    F=(c^(1-sigma))/(1-sigma); % The utility function
+end
+
+end
